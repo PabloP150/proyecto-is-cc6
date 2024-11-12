ca017e364291404a04ff92665f4c1b61f22165e0
Pablo Pineda <pablo.pineda@galileo.edu>
2024-11-12 08:23:51 -0600

    enciptación de contraseñas

--- taskmate-api/server.js
+++ taskmate-api/server.js
@@ -9,6 +9,7 @@ const edgesController = require('./controllers/edges.controller');
 const completeController = require('./controllers/complete.controller');
 const deleteController = require('./controllers/delete.controller');
 const usertaskController = require('./controllers/usertask.controller');
+const bcryptjs = require('bcryptjs');
 dotenv.config();
 
 const {
@@ -20,6 +21,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use((req, res, next) => {
     console.log(`Request client URL: ${req.get('host')}${req.originalUrl} >>>> ${SERVER_TAG}`);
     next();
